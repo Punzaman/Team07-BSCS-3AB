a24f06ff1b8f8c3c02f261ddf5c5c4d8bf939c72
Punzaman <kurkkeithkylepunzalan@gmail.com>
2021-12-14 16:53:21 +0800

    Updated DB Schema

--- phpwebsite v2.1/SQL File/phptest.sql
+++ phpwebsite v2.1/SQL File/phptest.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2021 at 01:53 AM
+-- Generation Time: Dec 14, 2021 at 09:49 AM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- PHP Version: 7.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `phptest`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lots`
+--
+
+CREATE TABLE `lots` (
+  `lot_id` int(11) NOT NULL,
+  `lot_num` int(11) NOT NULL,
+  `lot_status` enum('Available','Occupied') DEFAULT NULL,
+  `reserveID` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `lots`
+--
+
+INSERT INTO `lots` (`lot_id`, `lot_num`, `lot_status`, `reserveID`) VALUES
+(1, 1, 'Available', ''),
+(2, 2, 'Available', ''),
+(3, 3, 'Available', ''),
+(4, 4, 'Available', ''),
+(5, 5, 'Available', '');
 
 -- --------------------------------------------------------
 
@@ -56,7 +80,8 @@ INSERT INTO `reservation` (`reserveId`, `reserveDate`, `reservePark`, `reserveUs
 (15, '2021-12-25', '3PM', '2'),
 (16, '2021-12-20', '4PM', '2'),
 (17, '2021-12-08', '1PM', '2'),
-(18, '2021-12-11', '1PM', '2');
+(18, '2021-12-11', '1PM', '2'),
+(19, '2021-12-18', '4PM', '3');
 
 -- --------------------------------------------------------
 
@@ -80,7 +105,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `user_firstname`, `user_lastname`, `user_email`, `username`, `user_pwd`, `user_pwdrepeat`) VALUES
 (1, 'kenlord', 'pamatian', 'ken@gmail.com', 'kenlord55', '202cb962ac59075b964b07152d234b70', ''),
-(2, 'qwe', 'qwe', 'qwe@gmail.com', 'qwe', '76d80224611fc919a5d54f0ff9fba446', '');
+(2, 'qwe', 'qwe', 'qwe@gmail.com', 'qwe', '76d80224611fc919a5d54f0ff9fba446', ''),
+(3, 'kurk', 'van', 'kurk@gmail.com', 'kurk', '202cb962ac59075b964b07152d234b70', ''),
+(4, 'Keith', 'Punzalan', 'keith@gmail.com', 'keith', '202cb962ac59075b964b07152d234b70', ''),
+(5, 'kyle', 'punzalan', 'kyle@gmail.com', 'kyle', '202cb962ac59075b964b07152d234b70', '');
 
 -- --------------------------------------------------------
 
@@ -116,11 +144,18 @@ INSERT INTO `uservehicle` (`vehicle_id`, `vehicle_type`, `vehicle_model`, `plate
 (12, 'Mitsubishi', 'Eclipse Cross', 'ABC-123', 'BLUE', '2'),
 (13, 'Mitsubishi', 'L300', 'ABCD-123', 'GREEN', '2'),
 (14, 'MITSUBISHI', 'EL THREE', 'ABD-124', 'GREEN', '2'),
-(15, 'APPLE', 'CAR', 'APPLE-123', 'RED', '2');
+(15, 'APPLE', 'CAR', 'APPLE-123', 'RED', '2'),
+(16, 'Honda', 'Civic', 'STU-809', 'white', '3');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `lots`
+--
+ALTER TABLE `lots`
+  ADD PRIMARY KEY (`lot_id`);
 
 --
 -- Indexes for table `reservation`
@@ -145,22 +180,28 @@ ALTER TABLE `uservehicle`
 --
 
 --
+-- AUTO_INCREMENT for table `lots`
+--
+ALTER TABLE `lots`
+  MODIFY `lot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `reserveId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `reserveId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `uservehicle`
 --
 ALTER TABLE `uservehicle`
-  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
